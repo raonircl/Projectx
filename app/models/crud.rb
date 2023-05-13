eabcf12cfae304fe55765241deae8c72e791b695
raonircl <raoni.afi@gmail.com>
2023-05-12 22:09:59 -0300

    create controller

--- app/models/crud.rb
+++ app/models/crud.rb
@@ -1,4 +1,4 @@
 class Crud < ApplicationRecord
-  validates :name, presence: true, length: {minimum:6, maximum:50}
+  validates :name, presence: true, length: {minimum:3, maximum:50}
   validates :age, numericality: {only_integer: true, :greater_than_or_equal_to => 0}
 end
