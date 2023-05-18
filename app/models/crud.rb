007529a5f4ab1add5765fcb0f7462cad8850e5c0
raonircl <raoni.afi@gmail.com>
2023-05-18 18:00:35 -0300

    create index home, validate letters

--- app/models/crud.rb
+++ app/models/crud.rb
@@ -1,4 +1,5 @@
 class Crud < ApplicationRecord
   validates :name, presence: true, length: {minimum:3, maximum:50}
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :age, numericality: {only_integer: true, :greater_than_or_equal_to => 0}
 end
